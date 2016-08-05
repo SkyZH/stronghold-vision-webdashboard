878e34fb8bd1350d04d856a93b2935e1cacadf42
SkyZH <iSkyZH@gmail.com>
2016-08-05 16:56:46 +0800

    add spec

---
+++ src/app/app.component.e2e-spec.js
@@ -0,0 +1,18 @@
+describe('App', function () {
+
+  beforeEach(function () {
+    browser.get('/');
+  });
+
+  it('should have a title', function () {
+    expect(browser.getTitle()).toEqual("Zodiac 的视觉中枢");
+  });
+
+  it('should have <header>', function () {
+    expect(element(by.css('my-app header')).isPresent()).toEqual(true);
+  });
+
+  it('should have <main>', function () {
+    expect(element(by.css('my-app main')).isPresent()).toEqual(true);
+  });
+});
