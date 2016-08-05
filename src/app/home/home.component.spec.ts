4894ab0e55680eb01e1c19bc783be3466f59a2be
SkyZH <iSkyZH@gmail.com>
2016-08-05 17:32:49 +0800

    remove coverage

---
+++ src/app/home/home.component.spec.ts
@@ -0,0 +1,16 @@
+import {
+  it,
+  inject,
+  describe,
+  beforeEachProviders,
+} from '@angular/core/testing';
+
+// Load the implementations that should be tested
+import { HomeComponent } from './home.component';
+
+describe('Home', () => {
+  // provide our implementations or mocks to the dependency injector
+  beforeEachProviders(() => [
+    HomeComponent
+  ]);
+});
